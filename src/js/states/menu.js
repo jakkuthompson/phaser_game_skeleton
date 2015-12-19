3cb863ceeff44ce17a450d404bef19b54ae2e70a
Jack Thompson <jacktinternetboy@gmail.com>
2015-12-19 14:41:26 -0500

    background added

--- src/js/states/menu.js
+++ src/js/states/menu.js
@@ -7,16 +7,12 @@ module.exports = Menu;
 Menu.prototype = {
 
   create: function () {
-
+    this.asset = this.add.sprite(0,0, 'menuback');
+    this.asset.scale.x = 0.5;
+    this.asset.scale.y = 0.5;
     this.asset = this.add.sprite(25, 25, 'menulogo');
     this.asset.scale.x = 2;
     this.asset.scale.y = 2;
-
-    this.playbutton = this.add.sprite(500, 150, 'playbutton');
-    this.playbutton.scale.x = 0.5;
-    this.playbutton.scale.y = 0.5;
-
-
 
 
   },
@@ -25,7 +21,6 @@ Menu.prototype = {
   },
 
   onDown: function () {
-
+    this.game.state.start(playerState.currentLevel);
   }
 };
-
