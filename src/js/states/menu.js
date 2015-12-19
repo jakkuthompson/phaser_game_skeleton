968f8d03331e88b47dd670a2de971b2fcb4f76e0
Nathan <nathan.2002.rogers@gmail.com>
2015-12-19 13:24:50 -0500

    progress on menu

--- src/js/states/menu.js
+++ src/js/states/menu.js
@@ -7,12 +7,10 @@ module.exports = Menu;
 Menu.prototype = {
 
   create: function () {
-    var x = this.game.width / 2;
-    var y = this.game.height / 2;
 
     var style = { font: "65px Arial", fill: "#ffffff", align: "left" };
 
-    this.text = this.add.text(x - 300, y - 200, "Press to Start", style);
+    this.text = this.add.text(240, 200, "Press to Start", style);
 
   },
 
