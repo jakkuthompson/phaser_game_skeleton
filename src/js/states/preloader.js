0e132627d81428879022e7f1b9ce9a3b69a5426b
Nathan <nathan.2002.rogers@gmail.com>
2016-02-06 13:24:34 -0500

    added main character positions

--- src/js/states/preloader.js
+++ src/js/states/preloader.js
@@ -23,7 +23,7 @@ Preloader.prototype = {
     this.load.spritesheet('menucredits', 'assets/menu/creditsbutton.png', 338, 170);
     this.load.audio('bwing', 'assets/audio/bwing.mp3');
     this.load.audio('menutheme', 'assets/audio/Kawai Kitsune.mp3');
-    this.load.spritesheet('maincharacter', 'assets/game/Zephyr.png', 160, 160,2);
+    this.load.spritesheet('maincharacter', 'assets/game/Zephyr.png', 30, 45,15,2,2);
     this.load.tilemap('testroom', 'assets/game/test-room.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.image('tileset', 'assets/DungeonCrawl_ProjectUtumnoTileset.png');
     this.load.spritesheet('enemy', 'assets/game/walk - vanilla.png', 64,64);
